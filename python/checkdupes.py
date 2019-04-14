be95e5f213eab42e46ca381459e24d62b06911bb
Brutalmelon <koreypecha@aol.com>
2019-04-14 05:50:57 -0700

    updated duplicates

--- python/checkdupes.py
+++ python/checkdupes.py
@@ -11,16 +11,13 @@ from flask import request
 from flask import jsonify
 
 def checkdupes(checkID):
-    transactions_response = client.Transactions.get(access_token, start_date, end_date)
-    except plaid.errors.PlaidError as e:
-        return jsonify(format_error(e))
-    pretty_print_response(transactions_response)
-    for trans in transactions_response:
-        compareID = transactions_response['accounts'][0]['account_id']
+    for row in cursor.execute('SELECT findata.uID from findata')
+        compareID = row.uID
         if compareID.equals(checkID):
             return null
         else
-        compareID = trans['account_id']
+        for row in cursor.execute('SELECT findata.account_ID from findata')
+        compareID = row.account_ID
         if compareID.equals(checkID):
             return null
         else
